00d86f604dbcfa8fec82f819ee3dfd53de2c949d
Pepe <pgarciag93@gmail.com>
2024-10-24 15:10:57 +0200

    fix: update typo

--- 01_node/index.js
+++ 01_node/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 import sum from './sum.js';
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; chatset=utf-8', });
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8', });
   res.end('Aprendiendo en The Bridge! </br> La suma de 1+3 = ' + sum(1, 3));
 })
   .listen(8080);
